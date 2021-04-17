fd31bd8d73ed0c368d79692c681415df3e16b245
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-16 20:35:54 -0400

    adjusted styling added table template

--- employeedirectoryapp/src/components/EmployeeInfo/index.js
+++ employeedirectoryapp/src/components/EmployeeInfo/index.js
@@ -2,7 +2,32 @@ import React from "react";
 
 function EmployeeInfo(props){
   return(
+    <div>
   <p>{props.children}</p> 
+  
+  <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Picture</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Location</th>
+    </tr>
+  </thead>
+  
+  <tbody>
+
+    <tr>
+      <td>pic</td>
+      <td>Jacqui Ross</td>
+      <td>jross135</td>
+      <td>Royal Oak, Mi</td>
+    </tr>
+
+  </tbody>
+
+</table>
+</div>
   );
 
 }
