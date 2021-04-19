ee255d0b13b6d715803beaf9073a8eb8fb751afd
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-19 10:34:55 -0400

    filter and sort options added with valeus created

--- employeedirectoryapp/src/components/Search/index.js
+++ employeedirectoryapp/src/components/Search/index.js
@@ -3,16 +3,30 @@ import React from "react";
 
 function Search() {
   return(
+      <div>
+      {/* filter */}
 <div class="input-group mb-3">
   <div class="input-group-prepend">
-    <button class="btn btn-outline-secondary" type="button">Button</button>
+    <button class="btn btn-outline-secondary" type="button">Filter</button>
   </div>
   <select class="custom-select" id="inputGroupSelect03" aria-label="Example select with button addon">
-    <option selected>Choose...</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
+    <option selected>Filter by...</option>
+    <option value="male">Male</option>
+    <option value="female">Female</option>
   </select>
+</div>
+
+{/* sort */}
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <button class="btn btn-outline-secondary" type="button">Sort</button>
+  </div>
+  <select class="custom-select" id="inputGroupSelect03" aria-label="Example select with button addon">
+    <option selected>Sort by...</option>
+    <option value="name">Name</option>
+    <option value="location">Location</option>
+  </select>
+</div>
 </div>
   );
   }
